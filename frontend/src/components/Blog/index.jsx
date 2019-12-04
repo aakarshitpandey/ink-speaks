6250e56fa4d7ec1037aa425ff12124a46046b9ad
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-12-04 14:58:05 -0500

    moved stuff around

--- frontend/src/components/Blog/index.jsx
+++ frontend/src/components/Blog/index.jsx
@@ -12,8 +12,8 @@ export default class Blog extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            loading: false,
             blog: {},
+            loading: false,
             msg: null,
             id: null,
             likes: 0,
