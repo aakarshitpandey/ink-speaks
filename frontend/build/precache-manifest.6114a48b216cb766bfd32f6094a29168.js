392c5e3303e44d671a5f64c1267608c0f83f69b3
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-12-04 23:32:52 -0500

    Deploying on heroku

---
+++ frontend/build/precache-manifest.6114a48b216cb766bfd32f6094a29168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5be92b0642361dda64d32c0007e3ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbff1e72e1e3251b4c83",
+    "url": "/static/css/2.1ab83928.chunk.css"
+  },
+  {
+    "revision": "9dfabd3d4778e151f828",
+    "url": "/static/css/main.92ec3732.chunk.css"
+  },
+  {
+    "revision": "bbff1e72e1e3251b4c83",
+    "url": "/static/js/2.23c559e7.chunk.js"
+  },
+  {
+    "revision": "9dfabd3d4778e151f828",
+    "url": "/static/js/main.325d1be9.chunk.js"
+  },
+  {
+    "revision": "e1f58a59e6da6b490743",
+    "url": "/static/js/runtime-main.7045e9e7.js"
+  }
+]);
