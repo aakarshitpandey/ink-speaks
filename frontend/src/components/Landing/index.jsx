b4140e16fd0d067e983db1d06e43217bbd3c771a
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-05-14 02:36:22 +0530

    bugs

--- frontend/src/components/Landing/index.jsx
+++ frontend/src/components/Landing/index.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
 import SignUp from '../SignUp'
 import Posts from '../Posts'
+import { loggedIn } from '../../api/auth'
 
 class Landing extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    componentDidMount() {
+        if (loggedIn()) {
+            this.setState({ isLoggedIn: true })
+        }
+    }
+
     render() {
+        let trendingClassList = "uk-section uk-section-default col-md-6 col-lg-6 col-sm-12 post-container"
+        if (this.state.isLoggedIn) {
+            trendingClassList = "uk-section uk-section-default col-md-12 col-lg-12 col-sm-12 post-container"
+        } //end if
         return (
             <div className="uk-grid uk-padding-remove" uk-height-match uk-grid>
                 {/* <div className="uk-width-1-2@m uk-flex-first"> */}
-                <div id="trending-container" className="uk-section uk-section-default col-md-6 col-lg-6 col-sm-12 post-container">
+                <div id="trending-container" className={trendingClassList}>
                     <h1 className="uk-text-center"> Trending</h1>
                     <div className="uk-container uk-container-small">
                         <Posts general={true} />
@@ -16,11 +34,13 @@ class Landing extends Component {
                 </div>
                 {/* </div> */}
                 {/* <div className="uk-width-1-2@m"> */}
-                <div id="signUpContainer" className="uk-tile uk-tile-muted uk-tile-default col-md-6 col-lg-6 col-sm-12 ">
-                    <div className="uk-card uk-card-secondary uk-card-body">
-                        <SignUp />
+                {this.state.isLoggedIn ? <></> :
+                    <div id="signUpContainer" className="uk-tile uk-tile-muted uk-tile-default col-md-6 col-lg-6 col-sm-12 ">
+                        <div className="uk-card uk-card-secondary uk-card-body">
+                            <SignUp />
+                        </div>
                     </div>
-                </div>
+                }
                 {/* </div> */}
             </div>
 
