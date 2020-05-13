850422ff5dc44bcbb3405d7fe6a44524c484d0e2
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-05-14 01:23:30 +0530

    added login check to the landing page

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ce0be85927a9d4786d9bc1af08f16f2.js"
+  "/precache-manifest.4bc99126256d96b3923da22eb7141b02.js"
 );
 
 self.addEventListener('message', (event) => {
--- frontend/src/components/Landing/index.jsx
+++ frontend/src/components/Landing/index.jsx
@@ -1,29 +1,51 @@
 import React, { Component } from 'react'
 import SignUp from '../SignUp'
 import Posts from '../Posts'
+import { getUser } from '../../api/auth'
 
 class Landing extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = { isLoggedIn: false }
+    }
+
+    componentDidMount() {
+        const user = getUser()
+        if (user) {
+            console.log('user is logged in')
+            this.setState({ isLoggedIn: true })
+        } //end if
+    }
+
     render() {
         return (
-            <div className="uk-grid uk-padding-remove" uk-height-match uk-grid>
-                {/* <div className="uk-width-1-2@m uk-flex-first"> */}
-                <div id="trending-container" className="uk-section uk-section-default col-md-6 col-lg-6 col-sm-12 post-container">
-                    <h1 className="uk-text-center"> Trending</h1>
-                    <div className="uk-container uk-container-small">
-                        <Posts general={true} />
+            this.state.isLoggedIn ?
+                (<div className="uk-grid uk-padding-remove" uk-height-match uk-grid>
+                    <div id="trending-container" className="uk-section uk-section-default col-md-12 col-lg-12 col-sm-12 post-container">
+                        <h1 className="uk-text-center"> Trending</h1>
+                        <div className="uk-container uk-container-small">
+                            <Posts general={true} />
+                        </div>
                     </div>
-                </div>
-                {/* </div> */}
-                {/* <div className="uk-width-1-2@m"> */}
-                <div id="signUpContainer" className="uk-tile uk-tile-muted uk-tile-default col-md-6 col-lg-6 col-sm-12 ">
-                    <div className="uk-card uk-card-secondary uk-card-body">
-                        <SignUp />
+                </div>) :
+                (<div className="uk-grid uk-padding-remove" uk-height-match uk-grid>
+                    {/* <div className="uk-width-1-2@m uk-flex-first"> */}
+                    <div id="trending-container" className="uk-section uk-section-default col-md-6 col-lg-6 col-sm-12 post-container">
+                        <h1 className="uk-text-center"> Trending</h1>
+                        <div className="uk-container uk-container-small">
+                            <Posts general={true} />
+                        </div>
                     </div>
-                </div>
-                {/* </div> */}
-            </div>
-
+                    {/* </div> */}
+                    {/* <div className="uk-width-1-2@m"> */}
+                    <div id="signUpContainer" className="uk-tile uk-tile-muted uk-tile-default col-md-6 col-lg-6 col-sm-12 ">
+                        <div className="uk-card uk-card-secondary uk-card-body">
+                            <SignUp />
+                        </div>
+                    </div>
+                    {/* </div> */}
+                </div>)
         )
     }
 }
